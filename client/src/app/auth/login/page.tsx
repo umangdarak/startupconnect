294c6292327f532d484ee9b6adb9c0e981cd5dfb
Umang Darak <umangdarak@gmail.com>
2024-07-05 23:49:46 +0530

    ss

--- client/src/app/auth/login/page.tsx
+++ client/src/app/auth/login/page.tsx
@@ -5,7 +5,6 @@ import {
   Button,
   Card,
   Flex,
-  Radio,
   Text,
   TextField,
 } from "@radix-ui/themes";
@@ -13,18 +12,20 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { motion } from "framer-motion";
+
 export default function Login() {
   const router = useRouter();
   const [email, setEmail] = useState<string>();
   const [password, setPassword] = useState<string>();
   const [userType, setUserType] = useState<string>("");
-  const [showForm,setShowForm]=useState<boolean>(false);
+  const [showForm, setShowForm] = useState<boolean>(false);
   const [error, setError] = useState<{
     email?: string;
     password?: string;
     fetchError?: string;
   }>();
   const [alertVisible, setAlertVisible] = useState<boolean>(false);
+
   const validate = () => {
     const errors: { email?: string; password?: string } = {};
     if (!email) {
@@ -46,118 +47,122 @@ export default function Login() {
     const err = validate();
     setError(err);
     if (Object.keys(err).length === 0) {
-      
+      // Handle successful validation
     } else {
       setAlertVisible(true);
     }
   };
+
   return (
     <div className="flex flex-row justify-center items-center h-screen">
-    {!showForm ? (
-      <motion.div
-        initial={{ opacity: 1, }}
-        animate={{ opacity: showForm ? 0 : 1,scale: 1 }}
-        transition={{ duration: 1 }}
-        className="flex flex-col justify-center items-center border-solid border-customBlack border rounded-full p-10"
-      >
-        <Text className="text-customBlack font-mono">
-          Are you an Investor or an Entrepreneur/Startup Company?
-        </Text>
-        <div className="flex flex-row justify-between w-full px-6">
-          <Button
-            className="mb-5"
-            onClick={() => {
-              setUserType("Investor");
-              setShowForm(true);
-            }}
-          >
-            <Text className="text-white font-mono">Investor</Text>
-          </Button>
-          <Button
-            onClick={() => {
-              setUserType("Startup");
-              setShowForm(true);
-            }}
-          >
-            <Text className="text-white font-mono">Entrepreneur/Startup</Text>
-          </Button>
-        </div>
-      </motion.div>
-    ) : (
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-      >
-        <Box
-          width="600px"
-          className="border-black bg-slate-100 p-10 rounded-lg"
-          height="auto"
+      {!showForm ? (
+        <motion.div
+          initial={{ opacity: 1, scale: 0 }}
+          animate={{ opacity: showForm?0:1, scale: 1 }}
+          transition={{ duration: 0.5,ease:"linear" }}
+          className="flex flex-col justify-center items-center border-solid border-customBlack border rounded-full p-10"
         >
-          <Card size="4" variant="ghost">
-            <Box>
-              <TextField.Root
-                type="email"
-                placeholder="Enter your email"
-                className="my-3"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              {error?.email && (
-                <Text className="text-red-600">{error.email}</Text>
-              )}
-              <TextField.Root
-                type="password"
-                placeholder="Enter your password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              {error?.password && (
-                <Text className="text-red-600">{error.password}</Text>
-              )}
-              <div className="flex flex-row items-center w-full justify-center mt-4">
-                <Button className="my-3" size="3" onClick={handleSubmit}>
-                  <Text>Login</Text>
+          <Text className="text-customBlack font-mono">
+            Are you an Investor or an Entrepreneur/Startup Company?
+          </Text>
+          <div className="flex flex-row justify-between w-full px-6">
+            <Button
+              className="mb-5"
+              onClick={() => {
+                setUserType("Investor");
+                setShowForm(true);
+              }}
+            >
+              <Text className="text-white font-mono">Investor</Text>
+            </Button>
+            <Button
+              onClick={() => {
+                setUserType("Startup");
+                setShowForm(true);
+              }}
+            >
+              <Text className="text-white font-mono">Entrepreneur/Startup</Text>
+            </Button>
+          </div>
+        </motion.div>
+      ) : (
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.3 ,ease:"backIn"}}
+          className="flex flex-col justify-center items-center border-solid border-customBlack border rounded-md p-10"
+
+        >
+          <Button onClick={()=>{setUserType("");setShowForm(false);}}>Back</Button>
+          <Box
+            width="600px"
+            className="border-black bg-slate-100 p-10 rounded-lg"
+            height="auto"
+          >
+            <Card size="4" variant="ghost">
+              <Box>
+                <TextField.Root
+                  type="email"
+                  placeholder="Enter your email"
+                  className="my-3"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                {error?.email && (
+                  <Text className="text-red-600">{error.email}</Text>
+                )}
+                <TextField.Root
+                  type="password"
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {error?.password && (
+                  <Text className="text-red-600">{error.password}</Text>
+                )}
+                <div className="flex flex-row items-center w-full justify-center mt-4">
+                  <Button className="my-3" size="3" onClick={handleSubmit}>
+                    <Text>Login</Text>
+                  </Button>
+                </div>
+              </Box>
+              <Box className="mt-4">
+                <Text>
+                  Don't have an account?{" "}
+                  <Link href="/auth/register">
+                    <Text className="text-red-600">Register</Text>
+                  </Link>
+                </Text>
+              </Box>
+            </Card>
+          </Box>
+        </motion.div>
+      )}
+      {alertVisible && (
+        <AlertDialog.Root open={alertVisible}>
+          <AlertDialog.Content maxWidth="450px">
+            <AlertDialog.Title>Login Unsuccessful</AlertDialog.Title>
+            <AlertDialog.Description className="p-4 rounded">
+              <Flex className="flex flex-col">
+                {error?.email && <Text className="m-2">{error.email}</Text>}
+                {error?.password && (
+                  <Text className="m-2">{error.password}</Text>
+                )}
+                {error?.fetchError && (
+                  <Text className="m-2">{error.fetchError}</Text>
+                )}
+              </Flex>
+            </AlertDialog.Description>
+            <Flex className="flex flex-row justify-center items-center w-full">
+              <AlertDialog.Cancel onClick={() => setAlertVisible(false)}>
+                <Button variant="soft" color="gray">
+                  Ok
                 </Button>
-              </div>
-            </Box>
-            <Box className="mt-4">
-              <Text>
-                Don't have an account?{" "}
-                <Link href="/auth/register">
-                  <Text className="text-red-600">Register</Text>
-                </Link>
-              </Text>
-            </Box>
-          </Card>
-        </Box>
-      </motion.div>
-    )}
-    {alertVisible && (
-      <AlertDialog.Root open={alertVisible}>
-        <AlertDialog.Content maxWidth="450px">
-          <AlertDialog.Title>Login Unsuccessful</AlertDialog.Title>
-          <AlertDialog.Description className="p-4 rounded">
-            <Flex className="flex flex-col">
-              {error?.email && <Text className="m-2">{error.email}</Text>}
-              {error?.password && (
-                <Text className="m-2">{error.password}</Text>
-              )}
-              {error?.fetchError && (
-                <Text className="m-2">{error.fetchError}</Text>
-              )}
+              </AlertDialog.Cancel>
             </Flex>
-          </AlertDialog.Description>
-          <Flex className="flex flex-row justify-center items-center w-full">
-            <AlertDialog.Cancel onClick={() => setAlertVisible(false)}>
-              <Button variant="soft" color="gray">
-                Ok
-              </Button>
-            </AlertDialog.Cancel>
-          </Flex>
-        </AlertDialog.Content>
-      </AlertDialog.Root>
-    )}
-  </div>
-);
+          </AlertDialog.Content>
+        </AlertDialog.Root>
+      )}
+    </div>
+  );
 }
--- client/src/app/auth/register/page.tsx
+++ client/src/app/auth/register/page.tsx
@@ -1,26 +1,72 @@
 "use client";
-import { Button ,Text} from '@radix-ui/themes';
-import React, { useState } from 'react'
-import RegisterInvestor from './_RegisterInvestor';
-import RegisterStartup from './_RegisterStartup';
+import { Button, Text } from "@radix-ui/themes";
+import React, { useState } from "react";
+import RegisterInvestor from "./_RegisterInvestor";
+import RegisterStartup from "./_RegisterStartup";
+import { motion } from "framer-motion";
 
 export default function Register() {
-    const [userType, setUserType] = useState<string>("");
+  const [userType, setUserType] = useState<string>("");
+  const [showForm, setShowForm] = useState<boolean>(false);
 
   return (
-    <div className='flex flex-row justify-center items-center h-screen'> {userType?.length==0?
-        <div className="flex flex-col justify-center items-center border-solid border-customBlack border rounded-full p-10"
+    <div className="flex flex-row justify-center items-center h-screen">
+      {" "}
+      {!showForm ? (
+        <motion.div
+          initial={{ opacity: 1, scale: 0 }}
+          animate={{ opacity: showForm ? 0 : 1, scale: 1 }}
+          transition={{ duration: 0.5, ease: "linear" }}
+          className="flex flex-col justify-center items-center border-solid border-customBlack border rounded-full p-10"
         >
-           <Text className="text-customBlack font-mono">
-             Are you a Investor or an Entrepreneur/Startup Company?
-           </Text>
-           <div className="flex flex-row justify-between w-full px-6">
-           <Button className=" mb-5" onClick={(e)=>setUserType("Investor")}>
-             <Text className="text-white font-mono">Investor</Text>
-           </Button>
-           <Button className="" onClick={(e)=>setUserType("Startup")}>
-             <Text className="text-white font-mono">Entrepreneur/Startup</Text>
-           </Button></div>
-         </div>:userType==="Investor"?<RegisterInvestor />:<RegisterStartup />}</div>
-  )
+          <Text className="text-customBlack font-mono">
+            Are you an Investor or an Entrepreneur/Startup Company?
+          </Text>
+          <div className="flex flex-row justify-between w-full px-6">
+            <Button
+              className="mb-5"
+              onClick={() => {
+                setUserType("Investor");
+                setShowForm(true);
+              }}
+            >
+              <Text className="text-white font-mono">Investor</Text>
+            </Button>
+            <Button
+              onClick={() => {
+                setUserType("Startup");
+                setShowForm(true);
+              }}
+            >
+              <Text className="text-white font-mono">Entrepreneur/Startup</Text>
+            </Button>
+          </div>
+        </motion.div>
+      ) : (
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.3, ease: "backIn" }}
+        >
+          <Button
+            onClick={() => {
+              setUserType("");
+              setShowForm(false);
+            }}
+          >
+            Back
+          </Button>
+          {userType === "Investor" ? (
+            <div>
+              <RegisterInvestor />
+            </div>
+          ) : (
+            <div>
+              <RegisterStartup />
+            </div>
+          )}
+        </motion.div>
+      )}
+    </div>
+  );
 }
